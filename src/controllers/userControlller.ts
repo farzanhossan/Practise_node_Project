015b0ea064b560af010fc283dce0291f3c517a1e
farzanhossan <farzanhossans@gmail.com>
2020-11-24 12:47:27 +0600

    Winton working perfectly

--- src/controllers/userControlller.ts
+++ src/controllers/userControlller.ts
@@ -1,7 +1,9 @@
-import { usingSheetToJsonXLSX } from "../helper/fileRead";
-import { usingXLSX } from "../helper/fileWrite";
-import { Mail } from "../helper/mail";
-import { Winston } from "../helper/winston";
+import { usingSheetToJsonXLSX } from '../helper/fileRead';
+import { usingXLSX } from '../helper/fileWrite';
+import { Mail } from '../helper/mail';
+import { Winston } from '../helper/winston';
+const winstonInit = new Winston();
+const winston = winstonInit.logger('Checking');
 
 const sleep = async (milliseconds: any) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -10,13 +12,11 @@ export class UserController {
   /// Belongs-To-Many
   test = async (req: any, res: any, next: any) => {
     try {
-      const winstonInit = new Winston();
-      const winston = winstonInit.logger(req.baseUrl);
       winston.info(
         `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
       );
       return res.status(200).json({
-        success: "Success",
+        success: 'Success',
       });
     } catch (error) {
       next(error);
--- src/helper/winston.ts
+++ src/helper/winston.ts
@@ -1,17 +1,17 @@
-const { format } = require("winston");
-require("winston-daily-rotate-file");
-import { transports, createLogger } from "winston";
-import "winston-daily-rotate-file";
+const { format } = require('winston');
+require('winston-daily-rotate-file');
+import { transports, createLogger } from 'winston';
+import 'winston-daily-rotate-file';
 
 export const commonInfo = () => {
   const info = {
     filename: `log/%DATE%-combine.log`,
-    frequency: "5m",
-    datePattern: "YYYY-MM-DD-HH",
+    frequency: '5m',
+    datePattern: 'YYYY-MM-DD-HH',
     zippedArchive: true,
-    maxSize: "1k",
-    maxFiles: "5m",
-    level: "custom",
+    maxSize: '1k',
+    maxFiles: '5',
+    level: 'custom',
     levelOnly: false,
     handleExceptions: true,
     humanReadableUnhandledException: true,
@@ -35,7 +35,7 @@ export const formatInfo = (fileName: any) => {
     format.label({ label: fileName }),
     format.ms(),
     format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
     format.printf(
       (info: { level: any; timestamp: any; ms: any; message: any }) =>
@@ -48,20 +48,20 @@ export class Winston {
   private __errorTransport() {
     const common = commonInfo();
     common.filename = `log/error-%DATE%-error.log`;
-    common.level = "error";
+    common.level = 'error';
     return new (transports as any).DailyRotateFile(common);
   }
   private __combineTransport() {
     const common = commonInfo();
     common.filename = `log/combine-%DATE%-scheduler.log`;
-    common.level = "info";
+    common.level = 'info';
     return new (transports as any).DailyRotateFile(common);
   }
 
   logger(fileName: any) {
     return createLogger({
       levels: options(),
-      level: "custom",
+      level: 'custom',
       format: formatInfo(fileName),
       transports: [this.__errorTransport(), this.__combineTransport()],
       exitOnError: false,
